d28a9c8df86716121e70766ad43b18917814536c
jurek <jerzy.slomkowski@gmail.com>
2020-07-03 13:45:55 +0200

    change to california

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 import joblib
 
 app = Flask(__name__)
-model = joblib.load('boston_model.sav')
+model = joblib.load('california_model.sav')
 
 
 @app.route('/')
@@ -17,7 +17,7 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     return render_template(
-        'home.html', prediction_text=f"Expected value:{round(prediction[0]*1000,2)} USD")
+        'home.html', prediction_text=f"Wartość oczekiwana:{round(prediction[0]*100000,2)} USD")
 
 
 if __name__ == '__main__':
--- make_model.py
+++ make_model.py
@@ -1,13 +1,13 @@
 import pandas as pd
-from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
 import joblib
 
-headers = load_boston().feature_names[:3]
-# print(load_boston().DESCR)
-X, y = load_boston(return_X_y=True)
-X = pd.DataFrame(X[:, 0:3], columns=headers)
+headers = fetch_california_housing().feature_names
+X, y = fetch_california_housing(return_X_y=True)
+X = pd.DataFrame(X, columns=headers)
+X = X[['HouseAge', 'AveRooms', 'Population']]
 model = LinearRegression()
 model.fit(X, y)
 
-joblib.dump(model, 'boston_model.sav')
+joblib.dump(model, 'california_model.sav')
